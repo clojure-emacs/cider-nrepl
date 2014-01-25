dadf7c711c8e1a38f60ce52e882288483a2866af
Gary Trakhman <gary.trakhman@gmail.com>
2014-01-25 17:58:58 -0500

    clj/cljs completion support

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.nrepl "0.2.1"]])
+                 [clojure-complete "0.2.3"]
+                 [cljs-complete "0.1.1"]
+                 [org.clojure/tools.nrepl "0.2.3"]]
+
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.1.2"]]
+                   :repl-options {:nrepl-middleware [cider.nrepl.middleware.complete/wrap-complete]}
+                   :plugins [[com.cemerick/austin "0.1.3"]]}})
