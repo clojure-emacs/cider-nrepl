4cd8611deb250029c0a56a16656e76cb449d1dae
Gary Trakhman <gary.trakhman@gmail.com>
2014-03-09 00:25:59 -0500

    Test util.misc

---
+++ test/cider/nrepl/middleware/util/test_misc.clj
@@ -0,0 +1,22 @@
+(ns cider.nrepl.middleware.util.test-misc
+  (:require [clojure.test :refer :all]
+            [cider.nrepl.middleware.util.misc :as misc]))
+
+(deftest test-transform-value
+  (is (= (misc/transform-value (array-map
+                                :a "b"
+                                "c" :d
+                                :e false
+                                :f [:g :h :i]
+                                :j 4
+                                :k nil))
+         '("a" "b" "c" ":d" "e" "false" "f" (":g" ":h" ":i") "j" 4 "k" nil)))
+  (is (-> (misc/transform-value {:k (java.io.File. ".")})
+          second
+          java.io.File.
+          .isAbsolute)))
+
+(deftest test-as-sym
+  (is (= nil (misc/as-sym nil)))
+  (is (= 'WAT (misc/as-sym "WAT"))))
+
