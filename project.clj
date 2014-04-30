61b31b950ad7995c2c334186c5c7860c96b86aca
Bozhidar Batsov <bozhidar@batsov.com>
2014-05-01 00:07:09 +0300

    Bump compliment dep to 0.0.4

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compliment "0.0.3"]
+                 [compliment "0.0.4"]
                  [cljs-tooling "0.1.2"]
                  [org.tcrawley/dynapath "0.2.3"]
                  [org.clojure/tools.nrepl "0.2.3"]
