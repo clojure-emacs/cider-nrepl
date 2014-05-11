b7d1a38b449b9c228a03ac92c5ea1aa2267475c2
Bozhidar Batsov <bozhidar@batsov.com>
2014-05-11 23:48:29 +0300

    Bump compliment dep to 0.1.0

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :url "https://github.com/clojure-emacs/cider-nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[compliment "0.0.4"]
+  :dependencies [[compliment "0.1.0"]
                  [cljs-tooling "0.1.2"]
                  [org.tcrawley/dynapath "0.2.3"]
                  [org.clojure/tools.nrepl "0.2.3"]
