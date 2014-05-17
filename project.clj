fe20a629a453e2f5f1000fe3ca7556c13fb70301
Gary Trakhman <gary.trakhman@gmail.com>
2014-05-17 08:47:20 -0400

    Bump cljs-tooling version.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[compliment "0.1.0"]
-                 [cljs-tooling "0.1.2"]
+                 [cljs-tooling "0.1.3-SNAPSHOT"]
                  [org.tcrawley/dynapath "0.2.3"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/java.classpath "0.2.0"]
