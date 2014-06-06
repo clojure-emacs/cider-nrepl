2c5ca5bac2ef8f82ddf8465a5325ce8b64ed0e6d
Jeff Valk <jv@jeffvalk.com>
2014-06-06 12:06:11 -0400

    Ensure map keys are bencoded as strings.
    Add `:deprecated` to var metadata whitelist.

--- src/cider/nrepl/middleware/info.clj
+++ src/cider/nrepl/middleware/info.clj
@@ -18,7 +18,7 @@
     info))
 
 (def var-meta-whitelist
-  [:ns :name :doc :file :arglists :macro :protocol :line :column :static :added :resource])
+  [:ns :name :doc :file :arglists :macro :protocol :line :column :static :added :deprecated :resource])
 
 (defn- map-seq [x]
   (if (seq x)
