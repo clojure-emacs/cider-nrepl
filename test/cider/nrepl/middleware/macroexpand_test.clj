509aa17baf3753031a56f79feae2207faa1da678
Jeff Valk <jv@jeffvalk.com>
2014-07-03 18:01:18 -0400

    [Fix #93] Use consistent macroexpand tests.
    The `->` macro was rewritten for Clojure 1.6, so isn't the best choice
    for testing. Use something simple, having consistent macroexpansion
    across Clojure versions.

--- test/cider/nrepl/middleware/macroexpand_test.clj
+++ test/cider/nrepl/middleware/macroexpand_test.clj
@@ -3,26 +3,35 @@
         cider.nrepl.middleware.test-transport
         cider.nrepl.middleware.macroexpand))
 
+(def code
+  {:expr         "(while (while 1))"
+   :expanded     "(loop* [] (when (while 1) (recur)))"
+   :expanded-1   "(loop [] (when (while 1) (recur)))"
+   :expanded-all "(loop* [] (if (loop* [] (if 1 (do (recur)))) (do (recur))))"})
+
 (deftest test-macroexpand-1-op
   (let [transport (test-transport)]
     (macroexpansion-reply {:transport transport
                            :op "macroexpand-1"
-                           :code "(-> x f g)"
+                           :code (:expr code)
                            :suppress-namespaces true})
-    (is (= (messages transport) [{:value "(-> (-> x f) g)"} {:status #{:done}}]))))
+    (is (= (messages transport)
+           [{:value (:expanded-1 code)} {:status #{:done}}]))))
 
 (deftest test-macroexpand-op
   (let [transport (test-transport)]
     (macroexpansion-reply {:transport transport
                            :op "macroexpand"
-                           :code "(-> x f g)"
+                           :code (:expr code)
                            :suppress-namespaces true})
-    (is (= (messages transport) [{:value "(g (-> x f))"} {:status #{:done}}]))))
+    (is (= (messages transport)
+           [{:value (:expanded code)} {:status #{:done}}]))))
 
 (deftest test-macroexpand-all-op
   (let [transport (test-transport)]
     (macroexpansion-reply {:transport transport
                            :op "macroexpand-all"
-                           :code "(-> x f g)"
+                           :code (:expr code)
                            :suppress-namespaces true})
-    (is (= (messages transport) [{:value "(g (f x))"} {:status #{:done}}]))))
+    (is (= (messages transport)
+           [{:value (:expanded-all code)} {:status #{:done}}]))))
