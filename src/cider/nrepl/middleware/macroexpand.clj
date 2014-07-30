702a3798eb57b08df3590b58132643f1e8389d8d
Tassilo Horn <tsdh@gnu.org>
2014-07-30 08:34:31 +0200

    Support aliases instead of qnames in macroexpansion.

--- src/cider/nrepl/middleware/macroexpand.clj
+++ src/cider/nrepl/middleware/macroexpand.clj
@@ -2,6 +2,7 @@
   "Macroexpansion middleware."
   {:author "Bozhidar Batsov"}
   (:require [clojure.pprint :as pp]
+            [clojure.walk :as walk]
             [clojure.tools.nrepl.transport :as transport]
             [clojure.tools.nrepl.middleware :refer [set-descriptor!]]
             [clojure.tools.nrepl.misc :refer [response-for]]))
@@ -12,11 +13,38 @@
      (ns-resolve 'clojure.walk sym)
      (resolve sym))))
 
+(defn- aliasify-qualified-var-refs
+  "Takes a `form` (usually being the result of a macroexpansion) and replaces
+  any fully qualified var references with the namespace aliases defined for the
+  namespace `ns`."
+  [form ns]
+  (let [target-ns (the-ns ns)
+        alias2ns (ns-aliases target-ns)
+        ns2alias (apply hash-map (mapcat (fn [[a n]]
+                                           [(ns-name n) a])
+                                         alias2ns))]
+    (walk/prewalk
+     (fn [x]
+       (if (and (symbol? x) (namespace x))
+         (let [ns (symbol (namespace x))]
+           (if-let [alias (ns2alias ns)]
+             (symbol (str alias "/" (name x)))
+             x))
+         x))
+     form)))
+
 (defn macroexpansion [op code ns-name suppress-namespaces]
+  ;; suppress-namespaces can either be
+  ;;    'aliases => print aliases instead of qnames
+  ;;    falsy    => print qnames
+  ;;    truthy   => print simple names
   (let [expansion-fn (resolve-op op)
         ns (find-ns (symbol ns-name))
         ;; we have to do the macroexpansion in the proper ns context
         expansion (binding [*ns* ns] (expansion-fn (read-string code)))
+        [expansion suppress-namespaces] (if (= suppress-namespaces 'aliases)
+                                          [(aliasify-qualified-var-refs expansion ns) false]
+                                          [expansion suppress-namespaces])
         suppress-namespaces (boolean suppress-namespaces)]
     (with-out-str
       (pp/write expansion
