e0a39daf9875870d9e917c1cfe56027c54adf3e0
Tassilo Horn <tsdh@gnu.org>
2014-07-30 11:19:13 +0200

    Add tests for macroexpansion's suppress-namespaces feature.

--- test/cider/nrepl/middleware/macroexpand_test.clj
+++ test/cider/nrepl/middleware/macroexpand_test.clj
@@ -1,7 +1,9 @@
 (ns cider.nrepl.middleware.macroexpand-test
   (:use clojure.test
         cider.nrepl.middleware.test-transport
-        cider.nrepl.middleware.macroexpand))
+        cider.nrepl.middleware.macroexpand)
+  (:require [clojure.set :as set]
+            [clojure.string]))
 
 (def code
   {:expr         "(while (while 1))"
@@ -38,3 +40,54 @@
                            :suppress-namespaces true})
     (is (= (messages transport)
            [{:value (:expanded-all code)} {:status #{:done}}]))))
+
+;; Tests for the three different cider-macroexpansion-suppress-namespaces
+;; values: nil, t, and 'tidy
+
+(def my-set #{2 3})
+(defmacro ^:private tidy-test-macro []
+  `(deftest ~'test-foo
+     (is (clojure.string/blank? ""))
+     (is (= my-set (set/intersection #{1 2 3} #{2 3 4})))))
+
+(deftest test-macroexpand-1-op-suppress-namespaces-nil
+  ;; Tests that every var is properly qualified
+  (let [transport (test-transport)]
+    (macroexpansion-reply {:transport transport
+                           :op "macroexpand-1"
+                           :code "(tidy-test-macro)"
+                           :ns "cider.nrepl.middleware.macroexpand-test"
+                           :suppress-namespaces nil})
+    (let [[val stat] (messages transport)]
+      (is (= (:status stat) #{:done}))
+      (is (= (clojure.string/replace (:value val) #"[ \t\n]+" " ")
+             "(clojure.test/deftest test-foo (clojure.test/is (clojure.string/blank? \"\")) (clojure.test/is (clojure.core/= cider.nrepl.middleware.macroexpand-test/my-set (clojure.set/intersection #{1 2 3} #{2 3 4}))))")))))
+
+(deftest test-macroexpand-1-op-suppress-namespaces-true
+  ;; Tests that no var is qualified with its namespace
+  (let [transport (test-transport)]
+    (macroexpansion-reply {:transport transport
+                           :op "macroexpand-1"
+                           :code "(tidy-test-macro)"
+                           :ns "cider.nrepl.middleware.macroexpand-test"
+                           :suppress-namespaces true})
+    (let [[val stat] (messages transport)]
+      (is (= (:status stat) #{:done}))
+      (is (= (clojure.string/replace (:value val) #"[ \t\n]+" " ")
+             "(deftest test-foo (is (blank? \"\")) (is (= my-set (intersection #{1 2 3} #{2 3 4}))))")))))
+
+(deftest test-macroexpand-1-op-suppress-namespaces-tidy
+  ;; Tests that refered vars (deftest, is) and vars of the current ns (my-set)
+  ;; are not qualified.  Vars from other namespaces with an alias are
+  ;; referenced with the alias (set/intersection).  Every other var is fully
+  ;; qualified (clojure.string/blank?).
+  (let [transport (test-transport)]
+    (macroexpansion-reply {:transport transport
+                           :op "macroexpand-1"
+                           :code "(tidy-test-macro)"
+                           :ns "cider.nrepl.middleware.macroexpand-test"
+                           :suppress-namespaces "tidy"})
+    (let [[val stat] (messages transport)]
+      (is (= (:status stat) #{:done}))
+      (is (= (clojure.string/replace (:value val) #"[ \t\n]+" " ")
+             "(deftest test-foo (is (clojure.string/blank? \"\")) (is (= my-set (set/intersection #{1 2 3} #{2 3 4}))))")))))
