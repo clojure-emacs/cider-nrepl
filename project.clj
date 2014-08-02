451a3e3fb9f77ba2b03c65e835f76859133c163a
Bozhidar Batsov <bozhidar@batsov.com>
2014-08-02 20:30:30 +0300

    Bump tools.namespace dep to 0.2.5

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [org.tcrawley/dynapath "0.2.3"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/java.classpath "0.2.0"]
-                 [org.clojure/tools.namespace "0.2.4"]
+                 [org.clojure/tools.namespace "0.2.5"]
                  [org.clojure/tools.trace "0.7.8"]]
   :exclusions [org.clojure/clojure]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
