766d256c534aa3aa170ed939e8f8c83fb0ddbce7
Bozhidar Batsov <bozhidar@batsov.com>
2015-02-15 22:49:24 +0200

    [Fix #148] Bump cljfmt dep to 0.1.9
    This version no longer depends on fast-zip and potemkin.

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[compliment "0.2.1-SNAPSHOT"]
                  [cljs-tooling "0.1.3"]
-                 [cljfmt "0.1.7"]
+                 [cljfmt "0.1.9"]
                  [org.tcrawley/dynapath "0.2.3"]
                  [org.clojure/tools.nrepl "0.2.7"]
                  [org.clojure/java.classpath "0.2.0"]
