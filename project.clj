43f6f9f032c20f3731c4706ae8ad90771852ec2d
Bozhidar Batsov <bozhidar@batsov.com>
2015-04-23 00:07:31 +0300

    Revert "Revert "Bump the compliment dependency""
    This reverts commit 0e6c55eaab82caec7289c5bdf65aca7286228412.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                  [org.tcrawley/dynapath "0.2.3"]
                  [debugger "0.1.7"]
-                 ^:source-dep [compliment "0.2.1-SNAPSHOT"]
+                 ^:source-dep [compliment "0.2.2-SNAPSHOT"]
                  ^:source-dep [cljs-tooling "0.1.5-SNAPSHOT"]
                  ^:source-dep [cljfmt "0.1.10"]
                  ^:source-dep [org.clojure/java.classpath "0.2.2"]
