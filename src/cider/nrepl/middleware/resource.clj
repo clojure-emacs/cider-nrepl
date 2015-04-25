ae29c21ecf4efd96381b97ff8734826de11fbfa7
Bozhidar Batsov <bozhidar@batsov.com>
2015-04-25 23:46:38 +0300

    Add an op that lists all project resources

--- src/cider/nrepl/middleware/resource.clj
+++ src/cider/nrepl/middleware/resource.clj
@@ -3,11 +3,15 @@
             [clojure.tools.nrepl.transport :as transport]
             [clojure.tools.nrepl.middleware :refer [set-descriptor!]]
             [clojure.tools.nrepl.misc :refer [response-for]]
-            [cider.nrepl.middleware.util.misc :as u]))
+            [cider.nrepl.middleware.util.misc :as u]
+            [compliment.sources.resources :as r]))
 
 (defn resource-path [name]
   (when-let [resource (io/resource name)]
     (.getPath resource)))
+
+(defn resources-list []
+  (r/resources-by-prefix ""))
 
 (defn resource-reply
   [{:keys [name transport] :as msg}]
@@ -22,12 +26,26 @@
        transport
        (response-for msg (u/err-info e :resource-error))))))
 
+(defn resources-list-reply
+  [{:keys [name transport] :as msg}]
+  (try
+    (transport/send
+     transport
+     (response-for msg
+                   :resources-list (resources-list)
+                   :status :done))
+    (catch Exception e
+      (transport/send
+       transport
+       (response-for msg (u/err-info e :resources-list-error))))))
+
 (defn wrap-resource
   "Middleware that provides the path to resource."
   [handler]
   (fn [{:keys [op] :as msg}]
-    (if (= "resource" op)
-      (resource-reply msg)
+    (case op
+      "resource" (resource-reply msg)
+      "resources-list" (resources-list-reply msg)
       (handler msg))))
 
 (set-descriptor!
@@ -36,4 +54,7 @@
   {"resource"
    {:doc "Obtain the path to a resource."
     :requires {"name" "The name of the resource in question."}
-    :returns {"resource-path" "The file path to a resource."}}}})
+    :returns {"resource-path" "The file path to a resource."}}
+   "resources-list"
+   {:doc "Obtain a list of all resources on the classpath."
+    :returns {"resources-list" "The list of resources."}}}})
