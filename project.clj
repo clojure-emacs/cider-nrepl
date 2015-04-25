15bd4f3df7297ad073c0782e5368ed7169e5a21a
Michael Griffiths <mikey@cich.li>
2015-04-25 22:30:18 +0100

    Bump clojure 1.7 dep to 1.7.0-beta2

--- project.clj
+++ project.clj
@@ -63,7 +63,7 @@
 
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}
 
              :cljfmt {:plugins [[lein-cljfmt "0.1.10"]]}
              :eastwood {:plugins [[jonase/eastwood "0.2.1"]]
