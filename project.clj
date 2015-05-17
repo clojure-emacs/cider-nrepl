6dac5b24b5a039b3e2173e78d8cd5add27bd3c45
Bozhidar Batsov <bozhidar@batsov.com>
2015-05-18 00:00:17 +0300

    Bump the Clojure 1.7 dep to beta3

--- project.clj
+++ project.clj
@@ -64,7 +64,7 @@
 
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta3"]]}
 
              :cljfmt {:plugins [[lein-cljfmt "0.1.10"]]}
              :eastwood {:plugins [[jonase/eastwood "0.2.1"]]
