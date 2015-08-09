cf451db14abd7615597a51b2b53b4815d604c3e3
Michael Griffiths <mikey@cich.li>
2015-08-09 17:23:08 +0100

    Don't URL decode classpath entries
    As of this commit[1], clojure.java.classpath uses
    clojure.java.io/as-file, which does the decoding anyway.
    
    [1]: https://github.com/clojure/java.classpath/commit/5b6a17a268d6e3eb34cf9e690ba0bbb61e9fceb8
    
    Fixes #243.

--- src/cider/nrepl/middleware/classpath.clj
+++ src/cider/nrepl/middleware/classpath.clj
@@ -1,12 +1,11 @@
 (ns cider.nrepl.middleware.classpath
   (:require [clojure.java.classpath :as cp]
-            [clojure.tools.nrepl.transport :as transport]
             [clojure.tools.nrepl.middleware :refer [set-descriptor!]]
-            [clojure.tools.nrepl.misc :refer [response-for]])
-  (:import [java.net URLDecoder]))
+            [clojure.tools.nrepl.misc :refer [response-for]]
+            [clojure.tools.nrepl.transport :as transport]))
 
 (defn classpath []
-  (map #(URLDecoder/decode (str %) "utf-8") (cp/classpath)))
+  (map str (cp/classpath)))
 
 (defn classpath-reply
   [{:keys [transport] :as msg}]
