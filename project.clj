1296c277ee5fecee4cf5b63ab1efc95c9b553e8c
Michael Griffiths <mikey@cich.li>
2015-09-01 22:28:00 +0100

    Bump cljs-tooling dep to 0.1.9

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                  [org.tcrawley/dynapath "0.2.3"]
                  ^:source-dep [compliment "0.2.5-SNAPSHOT"]
-                 ^:source-dep [cljs-tooling "0.1.7"]
+                 ^:source-dep [cljs-tooling "0.1.9"]
                  ^:source-dep [cljfmt "0.3.0"]
                  ^:source-dep [org.clojure/java.classpath "0.2.2"]
                  ^:source-dep [org.clojure/tools.namespace "0.2.11"]
