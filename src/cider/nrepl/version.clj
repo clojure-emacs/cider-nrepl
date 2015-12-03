3ec64780f270e064a64655ee46f1a6fafd7fd004
Artur Malabarba <bruce.connor.am@gmail.com>
2015-12-03 13:45:05 +0000

    Ensure that print-method.clj actually gets required

--- src/cider/nrepl/version.clj
+++ src/cider/nrepl/version.clj
@@ -1,4 +1,7 @@
-(ns cider.nrepl.version)
+(ns cider.nrepl.version
+  ;; We require print-method here because `cider.nrepl.version`
+  ;; namespace is used by every connection.
+  (:require [cider.nrepl.print-method]))
 
 (def version
   "Current version of CIDER nREPL, map of :major, :minor, :incremental, and :qualifier."
