27745c435707d2237cc476527689b331b5419d70
Michael Griffiths <mikey@cich.li>
2015-12-11 20:36:01 +0000

    Add lein profile for clojure 1.8.0-RC3

--- project.clj
+++ project.clj
@@ -58,6 +58,9 @@
              :1.7 {:dependencies [^:replace [org.clojure/clojure "1.7.0"]
                                   [org.clojure/clojure "1.7.0" :classifier "sources"]
                                   [org.clojure/clojure "1.7.0" :classifier "javadoc"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC3"]
+                                  [org.clojure/clojure "1.8.0-RC3" :classifier "sources"]
+                                  [org.clojure/clojure "1.8.0-RC3" :classifier "javadoc"]]}
 
              :cljfmt {:plugins [[lein-cljfmt "0.3.0"]]
                       :cljfmt {:indents {as-> [[:inner 0]]}}}
