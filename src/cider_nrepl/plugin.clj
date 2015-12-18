e7b811a8a3dc61169b1ccfc328d22ad143d24972
Michael Griffiths <mikey@cich.li>
2015-12-18 21:52:39 +0000

    Don't inject the middleware if its version requirements aren't met
    Fixes #277.

--- src/cider_nrepl/plugin.clj
+++ src/cider_nrepl/plugin.clj
@@ -1,6 +1,6 @@
 (ns cider-nrepl.plugin
   (:require [clojure.java.io :as io]
-            [cider.nrepl :refer (cider-middleware)]))
+            [leiningen.core.main :as lein]))
 
 ;; Keep in sync with VERSION-FORM in project.clj
 (defn- version
@@ -15,11 +15,26 @@
     v))
 
 (defn middleware
-  [project]
-  (-> project
-      (update-in [:dependencies]
-                 (fnil into [])
-                 [['cider/cider-nrepl (version)]])
-      (update-in [:repl-options :nrepl-middleware]
-                 (fnil into [])
-                 cider-middleware)))
+  [{:keys [dependencies] :as project}]
+  (let [clojure-version-ok? (->> dependencies
+                                 (some (fn [[id version & {:as opts}]]
+                                         (and (= id 'org.clojure/clojure)
+                                              (lein/version-satisfies? version "1.7.0")))))
+        lein-version-ok? (lein/version-satisfies? (lein/leiningen-version) "2.5.2")]
+
+    (when-not clojure-version-ok?
+      (lein/warn "Warning: cider-nrepl requires Clojure 1.7 or greater."))
+    (when-not lein-version-ok?
+      (lein/warn "Warning: cider-nrepl requires Leiningen 2.5.2 or greater."))
+    (when-not (and clojure-version-ok? lein-version-ok?)
+      (lein/warn "Warning: cider-nrepl will not be included in your project."))
+
+    (cond-> project
+      (and clojure-version-ok? lein-version-ok?)
+      (-> (update-in [:dependencies]
+                     (fnil into [])
+                     [['cider/cider-nrepl (version)]])
+          (update-in [:repl-options :nrepl-middleware]
+                     (fnil into [])
+                     (do (require 'cider.nrepl)
+                         @(resolve 'cider.nrepl/cider-middleware)))))))
