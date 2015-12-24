5de54c1c0f5b89ebd5c89d98e194ce670edecf23
Michael Griffiths <mikey@cich.li>
2015-12-24 15:02:49 +0000

    Don't assume that a Clojure dependency is present in the project
    When running lein repl outside of a project, there will be no Clojure
    dependency. In this case, Leiningen 2.5.2+ (the versions we support)
    default to using Clojure 1.7.
    
    Fixes #279.

--- src/cider_nrepl/plugin.clj
+++ src/cider_nrepl/plugin.clj
@@ -16,17 +16,21 @@
 
 (defn middleware
   [{:keys [dependencies] :as project}]
-  (let [clojure-version-ok? (->> dependencies
-                                 (some (fn [[id version & {:as opts}]]
-                                         (and (= id 'org.clojure/clojure)
-                                              (lein/version-satisfies? version "1.7.0")))))
-        lein-version-ok? (lein/version-satisfies? (lein/leiningen-version) "2.5.2")]
+  (let [lein-version-ok? (lein/version-satisfies? (lein/leiningen-version) "2.5.2")
+        clojure-version (->> dependencies
+                             (some (fn [[id version & _]]
+                                     (when (= id 'org.clojure/clojure)
+                                       version))))
+        clojure-version-ok? (if (nil? clojure-version)
+                              ;; Lein 2.5.2+ uses Clojure 1.7 by default
+                              lein-version-ok?
+                              (lein/version-satisfies? clojure-version "1.7.0"))]
 
-    (when-not clojure-version-ok?
-      (lein/warn "Warning: cider-nrepl requires Clojure 1.7 or greater."))
     (when-not lein-version-ok?
       (lein/warn "Warning: cider-nrepl requires Leiningen 2.5.2 or greater."))
-    (when-not (and clojure-version-ok? lein-version-ok?)
+    (when-not clojure-version-ok?
+      (lein/warn "Warning: cider-nrepl requires Clojure 1.7 or greater."))
+    (when-not (and lein-version-ok? clojure-version-ok?)
       (lein/warn "Warning: cider-nrepl will not be included in your project."))
 
     (cond-> project
