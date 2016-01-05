6161f9db4fd80049c5cd3f2f6016f48dc449fe88
Bozhidar Batsov <bozhidar@batsov.com>
2016-01-05 10:09:30 +0200

    Release 0.10.1

--- src/cider/nrepl/version.clj
+++ src/cider/nrepl/version.clj
@@ -5,7 +5,7 @@
 
 (def version
   "Current version of CIDER nREPL, map of :major, :minor, :incremental, and :qualifier."
-  (let [version-string "0.10.1-snapshot"]
+  (let [version-string "0.10.1"]
     (assoc (->> version-string
                 (re-find #"(\d+)\.(\d+)\.(\d+)-?(.*)")
                 rest
