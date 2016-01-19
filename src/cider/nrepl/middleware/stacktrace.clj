330c4a4e19e7b6694a7873b32fe6802c39ebe4ef
Artur Malabarba <bruce.connor.am@gmail.com>
2016-01-19 12:51:39 +0000

    In stacktrace frames, also flag cider namespaces as tooling

--- src/cider/nrepl/middleware/stacktrace.clj
+++ src/cider/nrepl/middleware/stacktrace.clj
@@ -65,7 +65,7 @@
   to `clojure.lang.Compiler` or `clojure.tools.nrepl.*` as `:tooling` to
   distinguish compilation and nREPL middleware frames from user code."
   [frames]
-  (let [tool-regex #"clojure.lang.Compiler|clojure.tools.nrepl"
+  (let [tool-regex #"^clojure\.lang\.Compiler|^clojure\.tools\.nrepl|^cider\."
         tool? #(re-find tool-regex (or (:name %) ""))
         flag  #(update-in % [:flags] (comp set conj) :tooling)
         [user & tools] (partition-by (complement tool?) frames)]
