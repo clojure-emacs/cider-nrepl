53862e2b7acdcadd0b0d9f70f489b76f40197d7c
Artur Malabarba <bruce.connor.am@gmail.com>
2016-03-03 14:26:46 -0300

    Bump to version 0.12.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(def VERSION "0.11.0")
+(def VERSION "0.12.0-SNAPSHOT")
 
 (defproject cider/cider-nrepl VERSION
   :description "nREPL middlewares for CIDER"
--- src/cider/nrepl/version.clj
+++ src/cider/nrepl/version.clj
@@ -5,7 +5,7 @@
 
 (def version
   "Current version of CIDER nREPL, map of :major, :minor, :incremental, and :qualifier."
-  (let [version-string "0.11.0"]
+  (let [version-string "0.12.0-snapshot"]
     (assoc (->> version-string
                 (re-find #"(\d+)\.(\d+)\.(\d+)-?(.*)")
                 rest
