5106998c5d43f21e3dde7e1b7adbbef768c8ebea
cskksc <ckoparkar@live.in>
2016-05-17 22:22:12 +0530

    Support variables in eldoc op

--- test/common/cider/test_ns/first_test_ns.clj
+++ test/common/cider/test_ns/first_test_ns.clj
@@ -1,5 +1,9 @@
 (ns cider.test-ns.first-test-ns)
 
+(def some-test-var
+  "This is a test var used to check eldoc returned for a variable."
+  1)
+
 (defn same-name-testing-function
   "Multiple vars with the same name in different ns's. Used to test ns-list-vars-by-name."
   []
