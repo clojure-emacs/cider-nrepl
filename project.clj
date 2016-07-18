fda9244d657dfcb9a671af18bb7d9c90e1836001
Bozhidar Batsov <bozhidar@batsov.com>
2016-07-18 16:28:38 +0300

    Bump the fipp version

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                  [org.tcrawley/dynapath "0.2.4"]
                  ^:source-dep [mvxcvi/puget "1.0.0"]
-                 ^:source-dep [fipp "0.6.4"]
+                 ^:source-dep [fipp "0.6.5"]
                  ^:source-dep [compliment "0.3.0"]
                  ^:source-dep [cljs-tooling "0.2.0"]
                  ^:source-dep [cljfmt "0.4.1" :exclusions [org.clojure/clojurescript]]
