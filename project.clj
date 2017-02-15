a0d5a1668d79999bdaadee92a970ce0d957790b7
Dominic Monroe <dominic@juxt.pro>
2017-02-15 22:12:36 +0000

    Update to tools.namespace:0.3.0-alpha3
    Fixes #385

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  ^:source-dep [cljs-tooling "0.2.0"]
                  ^:source-dep [cljfmt "0.5.6" :exclusions [org.clojure/clojurescript]]
                  ^:source-dep [org.clojure/java.classpath "0.2.3"]
-                 ^:source-dep [org.clojure/tools.namespace "0.2.11"]
+                 ^:source-dep [org.clojure/tools.namespace "0.3.0-alpha3"]
                  ^:source-dep [org.clojure/tools.trace "0.7.9"]
                  ^:source-dep [org.clojure/tools.reader "0.10.0"]]
   :plugins [[thomasa/mranderson "0.4.7"]]
