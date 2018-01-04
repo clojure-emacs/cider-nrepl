072854134a00c16d9ff87320414c7b2ab6e95be6
Juan Monetta <jpmonettas@gmail.com>
2018-01-04 19:31:56 -0300

    [Fix #470] Add normalize-spec-form to normalize fn* into fn inside spec-form (#471)

--- test/clj/cider/nrepl/middleware/spec_test.clj
+++ test/clj/cider/nrepl/middleware/spec_test.clj
@@ -19,3 +19,21 @@
       (is (= (count filtered-spec-list)
              (count (:spec-list (session/message {:op "spec-list"
                                                   :filter-regex (str filter-regex ".+")}))))))))
+
+(deftest normalize-spec-form-test
+  (testing "All fn* subforms should be normalized"
+    (is (= (cider-spec/normalize-spec-form '(clojure.spec.alpha/fspec
+                                             :args (clojure.spec.alpha/and
+                                                    (fn* [p1__22097#]
+                                                         (clojure.core/< (:start p1__22097#) (:end p1__22097#)))
+                                                    (fn [%]
+                                                      (clojure.core/< (:start %) (:end %))))
+                                             :ret (fn* [p2__33098#]
+                                                       (clojure.core/> (:start p2__33098#) (:end p2__33098#)))
+                                             :fn nil))
+           '(clojure.spec.alpha/fspec
+             :args (clojure.spec.alpha/and
+                    (fn [%] (clojure.core/< (:start %) (:end %)))
+                    (fn [%] (clojure.core/< (:start %) (:end %))))
+             :ret (fn [%] (clojure.core/> (:start %) (:end %)))
+             :fn nil)))))
