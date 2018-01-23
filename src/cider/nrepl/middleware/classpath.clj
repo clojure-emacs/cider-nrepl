f291591bb57a307b4a0ac2b1cacb5512a04e6edf
Tianxiang Xiong <tianxiang.xiong@gmail.com>
2018-01-23 01:21:41 -0800

    Use `orchard.classpath`

--- src/cider/nrepl/middleware/classpath.clj
+++ src/cider/nrepl/middleware/classpath.clj
@@ -1,19 +1,9 @@
 (ns cider.nrepl.middleware.classpath
   (:require [cider.nrepl.middleware.util.error-handling :refer [with-safe-transport]]
-            [orchard.misc :as u]
-            [clojure.java.classpath :as cp]
-            [clojure.string :as str]))
-
-(defn classpath
-  "Return a list of classpath entries for the current project.
-  Takes into account the classpath trickery performed by Boot."
-  []
-  (if-let [classpath (u/boot-fake-classpath)]
-    (str/split classpath #":")
-    (map str (cp/classpath))))
+            [orchard.classpath :as cp]))
 
 (defn classpath-reply [msg]
-  {:classpath (classpath)})
+  {:classpath (map str (cp/classpath))})
 
 (defn handle-classpath [handler msg]
   (with-safe-transport handler msg
