f40f285144a50e30c62080dbbf09f5a13dd65917
Dominic Monroe <dominic@juxt.pro>
2018-04-16 17:20:51 +0100

    Extend selectors to include/exclude lists
    Fix #521

--- test/clj/cider/nrepl/middleware/test_filter_tests.clj
+++ test/clj/cider/nrepl/middleware/test_filter_tests.clj
@@ -1,13 +1,13 @@
-(ns cider.nrepl.middleware.test-selector-tests
-  "empty tests for tests testing the selector feature
+(ns cider.nrepl.middleware.test-filter-tests
+  "empty tests for tests testing the filter feature
   in `cider.nrepl.middleware.test-test` namespace"
   (:require [clojure.test :refer :all]))
 
 (deftest ^:smoke a-puff-of-smoke-test
   (is true "puff"))
 
-(deftest some-other-test
-  (is true "other"))
+(deftest ^:integration ^:smoke a-smokey-test
+  (is true "puff"))
 
 (deftest yet-an-other-test
   (is true "yet an other"))
--- test/clj/cider/nrepl/middleware/test_test.clj
+++ test/clj/cider/nrepl/middleware/test_test.clj
@@ -21,23 +21,33 @@
 (deftest only-smoke-test-run-test
   (testing "only test marked as smoke is run when test-all is used"
     (let [{:keys [results] :as test-result} (session/message {:op            "test-all"
-                                                              :selector "smoke"})
-          tests (keys (:cider.nrepl.middleware.test-selector-tests results))]
-      (is ((set (keys results)) :cider.nrepl.middleware.test-selector-tests) "ns that contains smoke is present")
+                                                              :include       ["smoke"]
+                                                              :exclude       ["integration"]})
+          tests (keys (:cider.nrepl.middleware.test-filter-tests results))]
+      (is ((set (keys results)) :cider.nrepl.middleware.test-filter-tests) "ns that contains smoke is present")
       (is (= 1 (count tests)) "only one test was run")
       (is (= :a-puff-of-smoke-test (first tests)) "only the test marked 'smoke' was run")))
-  (testing "only test marke as smoke is run when test-ns is used"
+  (testing "only test marked as smoke is run when test-ns is used"
     (let [{:keys [results] :as test-result} (session/message {:op            "test"
-                                                              :ns            "cider.nrepl.middleware.test-selector-tests"
-                                                              :selector "smoke"})
-          tests (keys (:cider.nrepl.middleware.test-selector-tests results))]
-      (is ((set (keys results)) :cider.nrepl.middleware.test-selector-tests) "ns that contains smoke is present")
+                                                              :ns            "cider.nrepl.middleware.test-filter-tests"
+                                                              :include       ["smoke"]
+                                                              :exclude       ["integration"]})
+          tests (keys (:cider.nrepl.middleware.test-filter-tests results))]
+      (is ((set (keys results)) :cider.nrepl.middleware.test-filter-tests) "ns that contains smoke is present")
       (is (= 1 (count tests)) "only one test was run")
       (is (= :a-puff-of-smoke-test (first tests)) "only the test marked 'smoke' was run")))
-  (testing "marked test is still run if selector is not used"
+  (testing "only test not marked as integration is run when test-ns is used"
     (let [{:keys [results] :as test-result} (session/message {:op            "test"
-                                                              :ns            "cider.nrepl.middleware.test-selector-tests"})
-          tests (keys (:cider.nrepl.middleware.test-selector-tests results))]
-      (is ((set (keys results)) :cider.nrepl.middleware.test-selector-tests) "ns that contains smoke is present")
+                                                              :ns            "cider.nrepl.middleware.test-filter-tests"
+                                                              :exclude       ["integration"]})
+          tests (keys (:cider.nrepl.middleware.test-filter-tests results))]
+      (is ((set (keys results)) :cider.nrepl.middleware.test-filter-tests) "ns that contains smoke is present")
+      (is (= 2 (count tests)) "only one test was run")
+      (is (= #{:a-puff-of-smoke-test :yet-an-other-test} (set tests)) "only the test marked 'smoke' was run")))
+  (testing "marked test is still run if filter is not used"
+    (let [{:keys [results] :as test-result} (session/message {:op            "test"
+                                                              :ns            "cider.nrepl.middleware.test-filter-tests"})
+          tests (keys (:cider.nrepl.middleware.test-filter-tests results))]
+      (is ((set (keys results)) :cider.nrepl.middleware.test-filter-tests) "ns that contains smoke is present")
       (is (< 1 (count tests)) "more tests were run")
-      (is ((set tests) :a-puff-of-smoke-test) "smoke test is still present without a selector"))))
+      (is ((set tests) :a-puff-of-smoke-test) "smoke test is still present without a filter"))))
