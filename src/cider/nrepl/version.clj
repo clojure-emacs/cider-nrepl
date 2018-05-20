6dd044faf61bc07493ae64853166ccf0ea8db164
Bozhidar Batsov <bozhidar@batsov.com>
2018-05-20 19:35:47 +0300

    Revert "Leverage versioneer to remove the version info duplication"
    This reverts commit 65fc896a9cad355a3134fa397db6e86968243600.
    
    Seems versioneer didn't work properly for some reason and the
    default version was returned instead.

--- src/cider/nrepl/version.clj
+++ src/cider/nrepl/version.clj
@@ -1,12 +1,11 @@
 (ns cider.nrepl.version
   ;; We require print-method here because `cider.nrepl.version`
   ;; namespace is used by every connection.
-  (:require [cider.nrepl.print-method]
-            [trptcolin.versioneer.core :as v]))
+  (:require [cider.nrepl.print-method]))
 
 (def version
   "Current version of CIDER nREPL, map of :major, :minor, :incremental, and :qualifier."
-  (let [version-string (v/get-version "cider" "cider-nrepl" "1.0.0")]
+  (let [version-string "0.18.0-snapshot"]
     (assoc (->> version-string
                 (re-find #"(\d+)\.(\d+)\.(\d+)-?(.*)")
                 rest
