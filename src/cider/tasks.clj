4a6be9f8a5e2a97bed95d959188f0e470f98aab1
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-06-16 00:27:31 -0700

    Add a simple nrepl-server task in cider.tasks (#532)
    It forwards to cider.nrepl.main/init passing options -b|--bind and -p|--port.

--- src/cider/tasks.clj
+++ src/cider/tasks.clj
@@ -11,8 +11,25 @@
   symbol."
   [m middleware MIDDLEWARE #{sym} "Name of the middleware to inject"]
   (if-let [default-middleware (resolve 'boot.repl/*default-middleware*)]
-    (do (util/dbug* "Boot's default middleware: %s\n" (vec @@default-middleware))
+    (do (util/dbug* "Current middleware: %s\n" (vec @@default-middleware))
         (swap! @default-middleware concat middleware)
         (util/dbug* "After cider-nrepl injection: %s\n" (vec @@default-middleware)))
     (util/dbug "Cannot resolve boot.repl/*default-middleware*, skipping middleware injection...\n"))
   identity)
+
+(deftask nrepl-server
+  "Start a nREPL server.
+
+  Optionally accepts port and host.
+
+  Note that the boot.repl/*default-middleware* atom is read for the list of the
+  middleware symbols."
+  [b bind ADDR      str   "The address server listens on."
+   p port PORT      int   "The port to listen on and/or connect to."]
+  (let [default-mws @@(resolve 'boot.repl/*default-middleware*)]
+    (util/dbug* "nREPL middleware: %s\n" (vec default-mws))
+    (boot.core/with-pass-thru [_]
+      (require 'cider-nrepl.main)
+      ((resolve 'cider-nrepl.main/init) {:middleware default-mws
+                                         :port port
+                                         :bind bind}))))
--- src/cider_nrepl/main.clj
+++ src/cider_nrepl/main.clj
@@ -8,32 +8,50 @@
   (require (symbol (namespace thing)))
   (resolve thing))
 
+(def resolve-mw-xf
+  (comp (map require-and-resolve)
+        (keep identity)))
+
 (defn- handle-seq-var
   [var]
   (let [x @var]
     (if (sequential? x)
-      (map require-and-resolve x)
+      (into [] resolve-mw-xf x)
       [var])))
+
+(def mw-xf
+  (comp (map symbol)
+        resolve-mw-xf
+        (mapcat handle-seq-var)))
 
 (defn- ->mw-list
   [middleware-var-strs]
-  (mapcat (comp handle-seq-var require-and-resolve symbol)
-          middleware-var-strs))
+  (into [] mw-xf middleware-var-strs))
 
-(defn- start-nrepl
-  [handler]
-  (let [nrepl-port (io/file ".nrepl-port")
+(defn start-nrepl
+  [opts]
+  (let [{:keys [handler middleware bind port]} opts
+
+        handler (cond-> (or handler nrepl.server/default-handler)
+                  middleware (apply (->mw-list middleware)))
+
         {:keys [server-socket port] :as server}
-        (nrepl.server/start-server :handler handler)
-        bind (-> server-socket (.getInetAddress) (.getHostName))]
-    (doto nrepl-port
+        (nrepl.server/start-server :handler handler
+                                   :bind bind
+                                   :port port)
+
+        bind
+        (-> server-socket (.getInetAddress) (.getHostName))]
+    (doto (io/file ".nrepl-port")
       (spit port)
       (.deleteOnExit))
     (println (format "nREPL server started on port %d on host %s - nrepl://%s:%d" port bind bind port))
     server))
 
 (defn init
-  [middleware-syms]
-  (start-nrepl (apply nrepl.server/default-handler (->mw-list middleware-syms)))
-  ;; Return nil so the value doesn't print
-  nil)
+  ([]
+   (init nil))
+  ([opts]
+   (start-nrepl opts)
+   ;; Return nil so the value doesn't print
+   nil))
