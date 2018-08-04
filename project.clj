e54c8e6ec4ea1309996b9dc860592d7a47b18014
Bozhidar Batsov <bozhidar@batsov.com>
2018-08-04 10:52:36 +0300

    Bump the piggieback dep

--- project.clj
+++ project.clj
@@ -94,7 +94,7 @@
                         :resource-paths ["test/resources"]
                         :test-paths ["test/clj"]}
              :test-cljs {:test-paths ["test/cljs"]
-                         :dependencies [[cider/piggieback "0.3.0"]]}
+                         :dependencies [[cider/piggieback "0.3.8"]]}
 
              :sysutils {:plugins [[lein-sysutils "0.2.0"]]}
 
