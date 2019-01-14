b8928a65204e595e53d8b2dfbc6cd168fc5f8478
Michael Griffiths <mikey@cich.li>
2019-01-14 22:53:07 +0000

    Bump the development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/cider-nrepl "0.20.0"
+(defproject cider/cider-nrepl "0.20.1-SNAPSHOT"
   :description "nREPL middlewares for CIDER"
   :url "https://github.com/clojure-emacs/cider-nrepl"
   :license {:name "Eclipse Public License"
