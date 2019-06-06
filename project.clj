97edf2a090ff25412d5bed82653ff15af11ea49e
Bozhidar Batsov <bozhidar@batsov.com>
2019-06-06 12:15:41 +0300

    Release 0.22.0-beta3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/cider-nrepl "0.22.0-beta2"
+(defproject cider/cider-nrepl "0.22.0-beta3"
   :description "A collection of nREPL middlewares designed to enhance Clojure editors."
   :url "https://github.com/clojure-emacs/cider-nrepl"
   :license {:name "Eclipse Public License"
