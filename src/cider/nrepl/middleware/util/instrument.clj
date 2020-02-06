6922c152eec298dc84ecf1e722ccc1fc5345915c
FiVo <finn.volkel@gmail.com>
2020-02-06 12:16:29 +0100

    Tests for map literal fix. Minor debug policy change.

--- src/cider/nrepl/middleware/util/instrument.clj
+++ src/cider/nrepl/middleware/util/instrument.clj
@@ -236,30 +236,26 @@
    (let [map-inner (fn [forms]
                      (map-indexed #(walk-indexed (conj coor %1) f %2)
                                   forms))
-         ;; Clojure uses array-maps up to some threshold (8 currently).
+         ;; Clojure uses array-maps up to some map size (8 currently).
          ;; So for small maps we take advantage of that, otherwise fall
          ;; back to the heuristic below.
          ;; Maps are unordered, but we can try to use the keys as order
          ;; hoping they can be compared one by one and that the user
-         ;; has specified them in that order. If that fails we fall
-         ;; back to simply instrumenting the values in the order they
-         ;; they appear in the map.
+         ;; has specified them in that order. If that fails we don't
+         ;; instrument the map. We also don't instrument sets.
          ;; This depends on Clojure implementation details.
+         walk-indexed-map (fn [map]
+                            (map-indexed (fn [i [k v]]
+                                           [(walk-indexed (conj coor (* 2 i)) f k)
+                                            (walk-indexed (conj coor (inc (* 2 i))) f v)])
+                                         map))
          result (cond
                   (map? form) (if (<= (count form) 8)
-                                (into {} (map-indexed (fn [i [k v]]
-                                                        [(walk-indexed (conj coor (* 2 i)) f k)
-                                                         (walk-indexed (conj coor (inc (* 2 i))) f v)])
-                                                      form))
+                                (into {} (walk-indexed-map form))
                                 (try
-                                  (into {} (map-indexed (fn [i [k v]]
-                                                          [(walk-indexed (conj coor (* 2 i)) f k)
-                                                           (walk-indexed (conj coor (inc (* 2 i))) f v)])
-                                                        (into (sorted-map) form)))
+                                  (into (sorted-map) (walk-indexed-map (into (sorted-map) form)))
                                   (catch Exception e
-                                    (into {} (map-indexed (fn [i [k v]]
-                                                            [k (walk-indexed (conj coor (inc (* 2 i))) f v)])
-                                                          form)))))
+                                    form)))
                   ;; Order of sets is unpredictable, unfortunately.
                   (set? form)  form
                   ;; Borrowed from clojure.walk/walk
