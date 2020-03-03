6f6e73bfb836827718534317f30eec73501706b3
Pranav Gajjewar <apbetahouse45@gmail.com>
2020-03-03 14:01:43 +0530

    [Fix #534] Address a couple of issues with the slurp middleware (#669)
    - Avoid a potential NPE when a content type is missing
    - Add special handling for directories

--- test/clj/cider/nrepl/middleware/slurp_test.clj
+++ test/clj/cider/nrepl/middleware/slurp_test.clj
@@ -26,3 +26,17 @@
     (t/is (= ["application/octet-stream" {}] (:content-type resp)))
     (t/is (str/starts-with? (:body resp) "#binary[location="))
     (t/is (str/ends-with? (:body resp) ",size=681]"))))
+
+(t/deftest test-directory
+  (let [resp (slurp-url-to-content+body
+              (.toString (.toURL (io/file "test"))))]
+    (t/is (= ["application/octet-stream" {}] (:content-type resp)))
+    (t/is (str/starts-with? (:body resp) "#binary[location="))
+    (t/is (str/ends-with? (:body resp) ",size=0]"))))
+
+(t/deftest test-url-content-type
+  ;; When a url gives content type as nil, then default content-type should be taken.
+  (let [resp (slurp-url-to-content+body "https://clojure.org/no-such-page")]
+    (t/is (= ["application/octet-stream" {}]))
+    (t/is (str/starts-with? (:body resp) "#binary[location="))
+    (t/is (str/ends-with? (:body resp) ",size=0]"))))
