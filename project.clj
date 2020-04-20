310ee41a11f38d0b7e9cd1ec5686b5f2803193b5
Bozhidar Batsov <bozhidar@batsov.com>
2020-04-20 09:54:49 +0300

    Release 0.25.0-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/cider-nrepl "0.25.0-SNAPSHOT"
+(defproject cider/cider-nrepl "0.25.0-alpha1"
   :description "A collection of nREPL middlewares designed to enhance Clojure editors."
   :url "https://github.com/clojure-emacs/cider-nrepl"
   :license {:name "Eclipse Public License"
