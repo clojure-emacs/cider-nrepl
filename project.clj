2587706dd7424951dd6b8bceecbe9e597fa7fdc0
vemv <vemv@users.noreply.github.com>
2021-10-03 11:15:00 +0200

    0.27.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/cider-nrepl "0.27.1"
+(defproject cider/cider-nrepl "0.27.2"
   :description "A collection of nREPL middleware designed to enhance Clojure editors."
   :url "https://github.com/clojure-emacs/cider-nrepl"
   :license {:name "Eclipse Public License"
