befad4d1ba14dba6cd9694c6fc12d816fad93c49
Roman Scherer <roman@burningswell.com>
2022-09-04 13:04:10 +0000

    Fix inspect tests
    Update the inspect test data to match the new section headers and the
    Datafy support added to Orchard.

--- test/clj/cider/nrepl/middleware/inspect_test.clj
+++ test/clj/cider/nrepl/middleware/inspect_test.clj
@@ -2,23 +2,112 @@
   (:require
    [cider.nrepl.middleware.inspect :as i]
    [cider.nrepl.test-session :as session]
+   [clojure.edn :as edn]
    [clojure.string :as string]
    [clojure.test :refer :all]))
 
-(def nil-result ["(\"nil\" (:newline))"])
+(def datafy?
+  (some? (resolve 'clojure.core.protocols/datafy)))
 
-(def var-result ["(\"Class\" \": \" (:value \"clojure.lang.Var\" 0) (:newline) \"Meta Information: \" (:newline) \"  \" (:value \":ns\" 1) \" = \" (:value \"clojure.core\" 2) (:newline) \"  \" (:value \":name\" 3) \" = \" (:value \"*assert*\" 4) (:newline) \"Value: \" (:value \"true\" 5))"])
+(def nil-result
+  '["nil" (:newline)])
+
+(def var-result
+  (cond-> '("Class"
+            ": " (:value "clojure.lang.Var" 0)
+            (:newline)
+            "Value: " (:value "true" 1)
+            (:newline)
+            (:newline)
+            "--- Meta Information:"
+            (:newline)
+            "  " (:value ":ns" 2) " = " (:value "clojure.core" 3)
+            (:newline)
+            "  " (:value ":name" 4) " = " (:value "*assert*" 5)
+            (:newline))
+    datafy? (concat '((:newline)
+                      "--- Datafy:"
+                      (:newline)
+                      "  " "0" ". " (:value "true" 6) (:newline)))))
 
 (def code "(sorted-map :a {:b 1} :c \"a\" :d 'e :f [2 3])")
 
-(def eval-result (eval (read-string code)))
+(def inspect-result
+  '("Class"
+    ": " (:value "clojure.lang.PersistentTreeMap" 0)
+    (:newline)
+    (:newline)
+    "--- Contents:"
+    (:newline)
+    "  " (:value ":a" 1) " = " (:value "{ :b 1 }" 2)
+    (:newline)
+    "  " (:value ":c" 3) " = " (:value "\"a\"" 4)
+    (:newline)
+    "  " (:value ":d" 5) " = " (:value "e" 6)
+    (:newline)
+    "  " (:value ":f" 7) " = " (:value "[ 2 3 ]" 8)
+    (:newline)))
 
-(def inspect-result ["(\"Class\" \": \" (:value \"clojure.lang.PersistentTreeMap\" 0) (:newline) \"Contents: \" (:newline) \"  \" (:value \":a\" 1) \" = \" (:value \"{ :b 1 }\" 2) (:newline) \"  \" (:value \":c\" 3) \" = \" (:value \"\\\"a\\\"\" 4) (:newline) \"  \" (:value \":d\" 5) \" = \" (:value \"e\" 6) (:newline) \"  \" (:value \":f\" 7) \" = \" (:value \"[ 2 3 ]\" 8) (:newline))"])
+(def push-result
+  '("Class"
+    ": " (:value "clojure.lang.PersistentArrayMap" 0)
+    (:newline)
+    (:newline)
+    "--- Contents:"
+    (:newline)
+    "  " (:value ":b" 1) " = " (:value "1" 2)
+    (:newline)
+    (:newline)
+    "--- Path:"
+    (:newline)
+    "  " ":a"))
 
-(def push-result ["(\"Class\" \": \" (:value \"clojure.lang.PersistentArrayMap\" 0) (:newline) \"Contents: \" (:newline) \"  \" (:value \":b\" 1) \" = \" (:value \"1\" 2) (:newline) (:newline) \"  Path: :a\")"])
+(def next-page-result
+  '("Class"
+    ": " (:value "clojure.lang.LazySeq" 0)
+    (:newline)
+    (:newline)
+    "--- Contents:"
+    (:newline)
+    "  " "..."
+    (:newline)
+    "  " "32" ". " (:value "32" 1)
+    (:newline)
+    "  " "33" ". " (:value "33" 2)
+    (:newline)
+    "  " "34" ". " (:value "34" 3)
+    (:newline)
+    (:newline)
+    "--- Page Info:"
+    (:newline)
+    "  " "Page size: 32, showing page: 2 of 2"))
 
-(def next-page-result ["(\"Class\" \": \" (:value \"clojure.lang.LazySeq\" 0) (:newline) \"Contents: \" (:newline) \"  ...\" (:newline) \"  \" \"32\" \". \" (:value \"32\" 1) (:newline) \"  \" \"33\" \". \" (:value \"33\" 2) (:newline) \"  \" \"34\" \". \" (:value \"34\" 3) (:newline) (:newline) \"  Page size: 32, showing page: 2 of 2\")"])
-(def first-page-result ["(\"Class\" \": \" (:value \"clojure.lang.LazySeq\" 0) (:newline) \"Contents: \" (:newline) \"  \" \"0\" \". \" (:value \"0\" 1) (:newline) \"  \" \"1\" \". \" (:value \"1\" 2) (:newline) \"  \" \"2\" \". \" (:value \"2\" 3) (:newline) \"  \" \"3\" \". \" (:value \"3\" 4) (:newline) \"  \" \"4\" \". \" (:value \"4\" 5) (:newline) \"  ...\" (:newline) \"  Page size: 5, showing page: 1 of ?\")"])
+(def first-page-result
+  '("Class"
+    ": " (:value "clojure.lang.LazySeq" 0)
+    (:newline)
+    (:newline)
+    "--- Contents:"
+    (:newline)
+    "  " "0" ". " (:value "0" 1)
+    (:newline)
+    "  " "1" ". " (:value "1" 2)
+    (:newline)
+    "  " "2" ". " (:value "2" 3)
+    (:newline)
+    "  " "3" ". " (:value "3" 4)
+    (:newline)
+    "  " "4" ". " (:value "4" 5)
+    (:newline)
+    "  " "..."
+    (:newline)
+    (:newline)
+    "--- Page Info:"
+    (:newline)
+    "  " "Page size: 5, showing page: 1 of ?"))
+
+(defn value [{:keys [value]}]
+  (edn/read-string (first value)))
 
 ;; integration tests
 
@@ -27,48 +116,48 @@
 (deftest nil-integration-test
   (testing "nil renders correctly"
     (is (= nil-result
-           (:value (session/message {:op      "eval"
-                                     :inspect "true"
-                                     :code    "nil"}))))))
+           (value (session/message {:op      "eval"
+                                    :inspect "true"
+                                    :code    "nil"}))))))
 
 (deftest pop-empty-integration-test
   (testing "popping an empty inspector renders nil"
     (is (= nil-result
-           (:value (session/message {:op "inspect-pop"}))))))
+           (value (session/message {:op "inspect-pop"}))))))
 
 (deftest pop-empty-idempotent-integration-test
   (testing "popping an empty inspector is idempotent"
     (is (= nil-result
-           (:value (do
-                     (session/message {:op "inspect-pop"})
-                     (session/message {:op "inspect-pop"})))))))
+           (value (do
+                    (session/message {:op "inspect-pop"})
+                    (session/message {:op "inspect-pop"})))))))
 
 (deftest push-empty-integration-test
   (testing "pushing an empty inspector index renders nil"
     (is (= nil-result
-           (:value (session/message {:op  "inspect-push"
-                                     :idx 1}))))))
+           (value (session/message {:op  "inspect-push"
+                                    :idx 1}))))))
 
 (deftest push-empty-idempotent-integration-test
   (testing "pushing an empty inspector index is idempotent"
     (is (= nil-result
-           (:value (do
-                     (session/message {:op  "inspect-push"
-                                       :idx 1})
-                     (session/message {:op  "inspect-push"
-                                       :idx 1})))))))
+           (value (do
+                    (session/message {:op  "inspect-push"
+                                      :idx 1})
+                    (session/message {:op  "inspect-push"
+                                      :idx 1})))))))
 
 (deftest refresh-empty-integration-test
   (testing "refreshing an empty inspector renders nil"
     (is (= nil-result
-           (:value (session/message {:op "inspect-refresh"}))))))
+           (value (session/message {:op "inspect-refresh"}))))))
 
 (deftest refresh-empty-idempotent-integration-test
   (testing "refreshing an empty inspector renders nil"
     (is (= nil-result
-           (:value (do
-                     (session/message {:op "inspect-refresh"})
-                     (session/message {:op "inspect-refresh"})))))))
+           (value (do
+                    (session/message {:op "inspect-refresh"})
+                    (session/message {:op "inspect-refresh"})))))))
 
 (deftest exception-integration-test
   (testing "eval op error handling"
@@ -150,99 +239,99 @@
 (deftest inspect-var-integration-test
   (testing "rendering a var"
     (is (= var-result
-           (:value (session/message {:op      "eval"
-                                     :inspect "true"
-                                     :code    "#'*assert*"}))))))
+           (value (session/message {:op      "eval"
+                                    :inspect "true"
+                                    :code    "#'*assert*"}))))))
 
 (deftest inspect-expr-integration-test
   (testing "rendering an expr"
     (is (= inspect-result
-           (:value (session/message {:op      "eval"
-                                     :inspect "true"
-                                     :code    code}))))))
+           (value (session/message {:op      "eval"
+                                    :inspect "true"
+                                    :code    code}))))))
 
 (deftest push-integration-test
   (testing "pushing a rendered expr inspector idx"
     (is (= push-result
-           (:value (do
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    code})
-                     (session/message {:op  "inspect-push"
-                                       :idx 2})))))))
+           (value (do
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    code})
+                    (session/message {:op  "inspect-push"
+                                      :idx 2})))))))
 
 (deftest next-page-integration-test
   (testing "jumping to next page in a rendered expr inspector"
     (is (= next-page-result
-           (:value (do
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    "(map identity (range 35))"})
-                     (session/message {:op "inspect-next-page"})))))))
+           (value (do
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    "(map identity (range 35))"})
+                    (session/message {:op "inspect-next-page"})))))))
 
 (deftest prev-page-integration-test
   (testing "jumping to previous page in a rendered expr inspector"
     (is (= first-page-result
-           (:value (do
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    "(map identity (range 35))"})
-                     (session/message {:op        "inspect-set-page-size"
-                                       :page-size 5})
-                     (session/message {:op "inspect-next-page"})
-                     (session/message {:op "inspect-prev-page"})))))))
+           (value (do
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    "(map identity (range 35))"})
+                    (session/message {:op        "inspect-set-page-size"
+                                      :page-size 5})
+                    (session/message {:op "inspect-next-page"})
+                    (session/message {:op "inspect-prev-page"})))))))
 
 (deftest pop-integration-test
   (testing "popping a rendered expr inspector"
     (is (= inspect-result
-           (:value (do
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    code})
-                     (session/message {:op  "inspect-push"
-                                       :idx 1})
-                     (session/message {:op "inspect-pop"})))))))
+           (value (do
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    code})
+                    (session/message {:op  "inspect-push"
+                                      :idx 1})
+                    (session/message {:op "inspect-pop"})))))))
 
 (deftest refresh-integration-test
   (testing "refreshing a rendered expr inspector"
     (is (= inspect-result
-           (:value (do
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    code})
-                     (session/message {:op "inspect-refresh"})))))))
+           (value (do
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    code})
+                    (session/message {:op "inspect-refresh"})))))))
 
 (deftest refresh-idempotent-integration-test
   (testing "refreshing a rendered expr inspector is idempotent"
     (is (= inspect-result
-           (:value (do
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    code})
-                     (session/message {:op "inspect-refresh"})
-                     (session/message {:op "inspect-refresh"})))))))
+           (value (do
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    code})
+                    (session/message {:op "inspect-refresh"})
+                    (session/message {:op "inspect-refresh"})))))))
 
 (deftest refresh-after-push-integration-test
   (testing "refreshing a rendered expr inspector after an idx is pushed"
     (is (= push-result
-           (:value (do
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    code})
-                     (session/message {:op  "inspect-push"
-                                       :idx 2})
-                     (session/message {:op "inspect-refresh"})))))))
+           (value (do
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    code})
+                    (session/message {:op  "inspect-push"
+                                      :idx 2})
+                    (session/message {:op "inspect-refresh"})))))))
 
 (deftest session-binding-integration-test
   (testing "session bindings can be inspected"
     (is (= inspect-result
-           (:value (do
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    code})
-                     (session/message {:op      "eval"
-                                       :inspect "true"
-                                       :code    "*1"})))))))
+           (value (do
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    code})
+                    (session/message {:op      "eval"
+                                      :inspect "true"
+                                      :code    "*1"})))))))
 
 (deftest page-size-integration-test
   (testing "page size can be changed in the eval op itself"
--- test/smoketest/project.clj
+++ test/smoketest/project.clj
@@ -1,6 +1,6 @@
 (defproject smoketest "0.1.0-SNAPSHOT"
   :dependencies [[nrepl "1.0.0"]
-                 [cider/cider-nrepl "0.28.6"]]
+                 [cider/cider-nrepl "0.28.5"]]
   :exclusions [org.clojure/clojure]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
