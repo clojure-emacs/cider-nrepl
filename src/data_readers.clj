0860960496b5eb42220d5231bef9e88b98331818
yuhan0 <qythium@gmail.com>
2023-06-14 09:41:32 +0800

    Rename reader macros -> break! / dbg!

--- src/data_readers.clj
+++ src/data_readers.clj
@@ -1,5 +1,5 @@
 {dbg cider.nrepl.middleware.debug/debug-reader
  break cider.nrepl.middleware.debug/breakpoint-reader
- exn cider.nrepl.middleware.debug/break-on-exception-reader
- dbgexn cider.nrepl.middleware.debug/debug-on-exception-reader
+ break! cider.nrepl.middleware.debug/break-on-exception-reader
+ dbg! cider.nrepl.middleware.debug/debug-on-exception-reader
  light cider.nrepl.middleware.enlighten/light-reader}
