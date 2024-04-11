86d54dee69b0338589c96fe439fa7649f69b400b
Alexander Yakushev <alex@bytopia.org>
2024-04-11 15:25:44 +0300

    Test against JDK22

--- test/smoketest/project.clj
+++ test/smoketest/project.clj
@@ -1,7 +1,6 @@
 (defproject smoketest "0.1.0-SNAPSHOT"
   :dependencies [[nrepl "1.0.0"]
                  [cider/cider-nrepl "RELEASE"]]
-  :exclusions [org.clojure/clojure]
   :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
              :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}
