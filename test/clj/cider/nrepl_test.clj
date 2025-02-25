9c32bfb5675b1da2ef54bdb4e09ef2af210fd874
Oleksandr Yakushev <alex@bytopia.org>
2025-02-25 15:54:06 +0200

    Disable warmup of orchard.java caches
    Also, warm up Compliment caches.

--- test/clj/cider/nrepl_test.clj
+++ test/clj/cider/nrepl_test.clj
@@ -7,9 +7,3 @@
   (is (let [v (System/getenv "CLOJURE_VERSION")]
         (println "Running on Clojure" (clojure-version))
         (or (nil? v) (.startsWith ^String (clojure-version) v)))))
-
-(deftest warmup-orchard-caches!
-  @sut/initializer ;; Prevent concurrent calls to `sut/warmup-orchard-caches!`
-  (testing "Can be executed without throwing exceptions"
-    (is (pos? (sut/warmup-orchard-caches!))
-        "Returns the keys count of the affected cache")))
