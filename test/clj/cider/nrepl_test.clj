b45298eccf2cf8e71ce1172474425e040d6b828b
Oleksandr Yakushev <alex@bytopia.org>
2025-10-09 13:06:48 +0300

    [ci] Test against multiple nREPL versions

--- test/clj/cider/nrepl_test.clj
+++ test/clj/cider/nrepl_test.clj
@@ -1,9 +1,14 @@
 (ns cider.nrepl-test
   (:require
    [cider.nrepl :as sut]
-   [clojure.test :refer [deftest is testing]]))
+   [clojure.test :refer [deftest is testing]]
+   [nrepl.version]))
 
-(deftest clojure-version-sanity-check
+(deftest versions-sanity-check
   (is (let [v (System/getenv "CLOJURE_VERSION")]
         (println "Running on Clojure" (clojure-version))
-        (or (nil? v) (.startsWith ^String (clojure-version) v)))))
+        (or (nil? v) (.startsWith ^String (clojure-version) v))))
+  (is (let [v (System/getenv "NREPL_VERSION")
+            nver (:version-string nrepl.version/version)]
+        (println "Running on nREPL" nver)
+        (or (nil? v) (.startsWith ^String nver v)))))
