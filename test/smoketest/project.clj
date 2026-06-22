e674716c74c3bc452f237ba511edb13bb79a5ab5
Bozhidar Batsov <bozhidar@batsov.dev>
2026-06-22 18:49:19 +0300

    Port the smoketest to tools.deps
    Replace the smoketest's lein project with a deps.edn plus a tools.build uberjar
    task. The just-installed cider-nrepl version is injected at build time, so the
    smoke test still exercises the real, freshly built artifact in a standalone
    uberjar.

--- test/smoketest/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject smoketest "0.1.0-SNAPSHOT"
-  :dependencies [[nrepl "1.0.0"]
-                 [cider/cider-nrepl "RELEASE"]]
-  :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
-             :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}
-             :1.12 {:dependencies [[org.clojure/clojure "1.12.0-rc1"]]}
-             :uberjar {:aot :all}}
-  :main smoketest.core)
